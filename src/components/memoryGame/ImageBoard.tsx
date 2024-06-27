df44bab422c5f48310eff03b3ae791d93433e34f
GeneralSting <pavo.skocibusic@gmail.com>
2024-06-27 20:15:28 +0200

    full game logic

--- src/components/memoryGame/ImageBoard.tsx
+++ src/components/memoryGame/ImageBoard.tsx
@@ -1,13 +1,66 @@
-import { FC, useState } from "react";
+import { FC, useCallback, useEffect, useState } from "react";
 import { ImageType } from "./images";
+import { useLocation } from "react-router-dom";
 
-type ImageBoradProps = {
+type ImageBoardProps = {
   initialImages: ImageType[];
+  resetGame: () => void;
 };
 
-const ImageBoard: FC<ImageBoradProps> = ({ initialImages }) => {
+type GameResult = 0 | 1 | 2 | 3;
+
+const ImageBoard: FC<ImageBoardProps> = ({ initialImages, resetGame }) => {
   const [images, setImages] = useState<ImageType[]>(initialImages);
   const [firstFlipped, setFirstFlipped] = useState<number | null>(null);
+  const [clickEnabled, setClickEnabled] = useState<boolean>(true);
+  const [pairsFound, setPairsFound] = useState<number>(0);
+  const [gameResult, setGameResult] = useState<GameResult>(0);
+  const [time, setTime] = useState({ minutes: 0, seconds: 0 });
+  const [isRunning, setIsRunning] = useState(false);
+  const location = useLocation(); // Get the location object
+
+  const getTime = useCallback(() => {
+    const searchParams = new URLSearchParams(location.search);
+    const minutesParam = searchParams.get("minutes");
+    const secondsParam = searchParams.get("seconds");
+    const minutes = minutesParam !== null ? parseInt(minutesParam, 10) : 0;
+    const seconds = secondsParam !== null ? parseInt(secondsParam, 10) : 0;
+
+    if (minutes !== 0 || seconds !== 0) {
+      setTime({ minutes, seconds });
+    }
+  }, [location.search]);
+
+  useEffect(() => {
+    getTime();
+  }, [getTime]);
+
+  useEffect(() => {
+    let timer: ReturnType<typeof setInterval>;
+    if (isRunning) {
+      timer = setInterval(() => {
+        setTime((prevTime) => {
+          const { minutes, seconds } = prevTime;
+          console.log(seconds);
+          if (minutes === 0 && seconds === 0) {
+            clearInterval(timer!);
+            alert("Time is up!");
+            setIsRunning(false);
+            setClickEnabled(false);
+            setGameResult(3);
+            return { minutes: 0, seconds: 0 };
+          } else if (seconds > 0) {
+            return { minutes, seconds: seconds - 1 };
+          } else if (seconds === 0 && minutes > 0) {
+            return { minutes: minutes - 1, seconds: 59 };
+          }
+          // Ensure a default return value
+          return prevTime; // Return previous state if none of the conditions match
+        });
+      }, 1000);
+    }
+    return () => clearInterval(timer);
+  }, [isRunning, time]);
 
   const numberOfImages = images.length;
   const columns = Math.ceil(Math.sqrt(numberOfImages));
@@ -34,40 +87,81 @@ const ImageBoard: FC<ImageBoradProps> = ({ initialImages }) => {
   };
 
   const handleImageClick = (index: number) => {
+    if (!isRunning) {
+      if (time.minutes !== 0 || time.seconds !== 0) {
+        setIsRunning(true);
+      }
+      setGameResult(1);
+    }
     setImages(showImage(index));
     if (firstFlipped === null) {
       setFirstFlipped(index);
     } else {
       if (images[firstFlipped].name !== images[index].name) {
+        setClickEnabled(false);
         setTimeout(() => {
           setImages(hideImages(firstFlipped, index));
+          setClickEnabled(true);
         }, 1000);
+      } else {
+        setPairsFound(pairsFound + 1);
       }
       setFirstFlipped(null);
     }
   };
 
+  useEffect(() => {
+    if (pairsFound === images.length / 2) {
+      setGameResult(2);
+      setIsRunning(false);
+    }
+  }, [images.length, pairsFound]);
+
+  useEffect(() => {
+    setImages(initialImages);
+    setFirstFlipped(null);
+    setGameResult(0);
+    setIsRunning(false);
+    setPairsFound(0);
+    getTime();
+    setClickEnabled(true);
+  }, [getTime, initialImages]);
+
   return (
-    <div
-      className="imagesWrapper"
-      style={{
-        display: "grid",
-        gridTemplateColumns: `repeat(${columns}, 1fr)`,
-        gridTemplateRows: `repeat(${rows}, 1fr)`,
-        gap: "10px",
-      }}
-    >
-      {images.map((image, index) =>
-        image.show ? (
-          <img src={image.url} key={index} alt={`image-${index}`} />
-        ) : (
-          <div
-            key={index}
-            className="imagePlaceholder"
-            onClick={() => handleImageClick(index)}
-          ></div>
-        )
-      )}
+    <div className="container">
+      <div
+        className="imagesWrapper"
+        style={{
+          display: "grid",
+          gridTemplateColumns: `repeat(${columns}, 1fr)`,
+          gridTemplateRows: `repeat(${rows}, 1fr)`,
+          gap: "10px",
+        }}
+      >
+        {images.map((image, index) =>
+          image.show ? (
+            <img src={image.url} key={index} alt={`image-${index}`} />
+          ) : (
+            <div
+              key={index}
+              className="imagePlaceholder"
+              onClick={(e) =>
+                clickEnabled ? handleImageClick(index) : e.preventDefault()
+              }
+            ></div>
+          )
+        )}
+      </div>
+      <div className="sidebar">
+        <div>
+          {String(time.minutes).padStart(2, "0")}:
+          {String(time.seconds).padStart(2, "0")}
+        </div>
+        <button onClick={resetGame}>reset game</button>
+        {gameResult === 0 && <div>Open first image to start</div>}
+        {gameResult === 2 && <div>Win</div>}
+        {gameResult === 3 && <div>Time is up!</div>}
+      </div>
     </div>
   );
 };
--- src/components/memoryGame/MemoryGame.tsx
+++ src/components/memoryGame/MemoryGame.tsx
@@ -1,16 +1,32 @@
 import "./MemoryGame.css";
 import { shuffle } from "lodash";
 import ImageBoard from "./ImageBoard";
-import { FC } from "react";
+import { FC, useCallback, useEffect, useState } from "react";
 import { ImageType } from "./images";
 
 type Props = {
   images: ImageType[];
 };
 
-const MemoryGame: FC<Props> = ({images}) => {
-  const gameImages = shuffle([...images, ...images]);
-  return <ImageBoard initialImages={gameImages} />;
+const MemoryGame: FC<Props> = ({ images }) => {
+  const [shuffledImages, setShuffledImages] = useState<ImageType[]>([]);
+
+  const shuffleImages = useCallback(() => {
+    const gameImages = shuffle([...images, ...images]);
+    setShuffledImages(gameImages);
+  }, [images]);
+
+  useEffect(() => {
+    shuffleImages();
+  }, [shuffleImages]);
+
+  return (
+    <>
+      {shuffledImages.length !== 0 && (
+        <ImageBoard initialImages={shuffledImages} resetGame={shuffleImages} />
+      )}
+    </>
+  );
 };
 
 export default MemoryGame;
